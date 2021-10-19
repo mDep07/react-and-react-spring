6e061d7dadae0b1db51ae746cdaeabe7d714a76a
mDep07 <mdepdma@gmail.com>
2021-10-19 17:35:58 -0300

    Probando

--- src/App.js
+++ src/App.js
@@ -25,28 +25,54 @@ const Input = ({ handleChangeText }) => {
   return <StyledInput value={text} onChange={onChangeValue} />;
 };
 
-const Paragraph = ({ children }) => {
+const ParagraphAnimated = ({ children }) => {
   const [styles, animate] = useSpring(() => ({
     from: { opacity: 0, y: -5 },
     to: { opacity: 1, y: 0 },
   }));
 
   useEffect(() => {
-    console.log(children);
     animate({ from: { opacity: 0, y: -5 }, to: { opacity: 1, y: 0 } });
   }, [children]);
 
   return <StyledParagraph style={styles}>{children}</StyledParagraph>;
 };
 
+const ParagraphAnimated2 = ({ children }) => {
+  const [styles, animate] = useSpring(() => ({
+    from: { y: -5 },
+    to: { y: 0 },
+  }));
+
+  useEffect(() => {
+    console.log(children.split(''));
+    animate({ from: { y: -5 }, to: { y: 0 } });
+  }, []);
+
+  return (
+    <StyledParagraph>
+      {children.split('').map((letter, index) => (
+        <span key={index} style={styles}>
+          {letter}
+        </span>
+      ))}
+    </StyledParagraph>
+  );
+};
+
 export default function App() {
-  const [text, setText] = useState('a');
+  const [text, setText] = useState('');
+  const [start, setStart] = useState(true);
   const changeText = (text) => setText(text);
+
+  const handleStartAnimation = () => setStart(!start);
 
   return (
     <div style={{ textAlign: 'center' }}>
       <Input handleChangeText={changeText} />
-      <Paragraph>{text}</Paragraph>
+      <ParagraphAnimated>{text}</ParagraphAnimated>
+      <button onClick={handleStartAnimation}>{start ? 'stop' : 'start'}</button>
+      {start && <ParagraphAnimated2>{text}</ParagraphAnimated2>}
     </div>
   );
 }
