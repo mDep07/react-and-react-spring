7b636c3b6fe63a327acc7e28d48e46dbbf29e00f
mDep07 <mdepdma@gmail.com>
2021-10-19 15:08:43 -0300

    Typing animation

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
+import styled from 'styled-components';
 import { useSpring, animated } from 'react-spring';
 
-const AnimatedComponent = () => {
-  const props = useSpring({ to: { opacity: 1 }, from: { opacity: 0 } });
-  return <animated.div style={props}>I will fade in</animated.div>;
+const StyledInput = styled(animated.input)`
+  padding: 10px 15px;
+  font-size: 20px;
+`;
+
+const StyledParagraph = styled(animated.p)`
+  font-size: 45px
+`;
+
+const Input = ({ handleChangeText }) => {
+  const [text, setText] = useState('');
+
+  const onChangeValue = (input) => {
+    const { value } = input.target;
+    setText(value);
+    handleChangeText(value);
+  };
+
+  return <StyledInput value={text} onChange={onChangeValue} />;
+};
+
+const Paragraph = ({ children }) => {
+  const [styles, animate] = useSpring(() => ({
+    from: { opacity: 0, y: -5 },
+    to: { opacity: 1, y: 0 },
+  }));
+
+  useEffect(() => {
+    console.log(children);
+    animate({ from: { opacity: 0, y: -5 }, to: { opacity: 1, y: 0 } });
+  }, [children]);
+
+  return <StyledParagraph style={styles}>{children}</StyledParagraph>;
 };
 
 export default function App() {
+  const [text, setText] = useState('a');
+  const changeText = (text) => setText(text);
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div style={{ textAlign: 'center' }}>
+      <Input handleChangeText={changeText} />
+      <Paragraph>{text}</Paragraph>
     </div>
   );
 }
